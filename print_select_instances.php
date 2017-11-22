b51e7f79d8b27e488e0ae83a697fc7fe74149f89
micafer <micafer1@upv.es>
2017-11-22 08:48:33 +0100

    Order instance types

--- print_select_instances.php
+++ print_select_instances.php
@@ -1,4 +1,12 @@
 <?php
+	function cmp($a, $b)
+	{
+		if (strlen($a) == strlen($b)) {
+			return strcmp($a, $b);
+		}
+		return (strlen($a) < strlen($b)) ? -1 : 1;
+	}
+
     $return_string = "";
     $return_string .= "<select name=\"front-fedcloud\" id=\"front-fedcloud\" data-placeholder=\"--Select one--\" style=\"width:350px;\" class=\"chzn-select form-control\" data-validate=\"drop_down_validation\"><option value=\"\"></option>";
 
@@ -7,7 +15,7 @@
 		$selectOption = $_POST['endpointfedcloud']; 
         $vo = $_POST['vofedcloud']; 
 		exec('python EGI_AppDB.py instances ' . $selectOption . " " . $vo, $instances);
-		asort($instances);
+		usort($instances, "cmp");
 		foreach ($instances as $instance) {
                                 list($inst_desc, $inst_name) = explode(";", $instance);
                                 $return_string .= "<option value=\"" . $inst_name . "\">" . $inst_desc . "</option>";
