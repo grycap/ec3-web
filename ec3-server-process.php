40ba06faa3ee8728a98941e83bab51248149e940
amcaar <amcaar@i3m.upv.es>
2018-05-31 12:56:14 +0200

    modift clustername format

--- ec3-server-process.php
+++ ec3-server-process.php
@@ -193,7 +193,7 @@ if($_POST){
         $rand = random_string(5);
     }
     //$name = "cluster_" . $rand;
-    $name = $cluster_name . $user_sub;
+    $name = $cluster_name . "_" . $user_sub;
     $lrms = strtolower($lrms);
     $sw = strtolower($sw);
 
