f49da3d3aa74d489d8a96ac424ec91badf7e8242
micafer <micafer1@upv.es>
2019-10-07 13:09:12 +0200

    workaround for recas site

--- ec3-server-process.php
+++ ec3-server-process.php
@@ -81,7 +81,7 @@ function generate_auth_file_fedcloud($endpoint, $clustername) {
     $gestor = fopen($auth, "w");
 #    fwrite($gestor, "id = occi; type = OCCI; proxy = " . $proxy . "; host = " . $endpoint . PHP_EOL);
     $tenant = "openid";
-    if (strpos($endpoint, "recas.ba.infn.it") $tenant = "oidc";
+    if (strpos($endpoint, "recas.ba.infn.it")) $tenant = "oidc";
     fwrite($gestor, "id = egi; type = OpenStack; host = " . $endpoint . "; username = egi.eu; auth_version = 3.x_oidc_access_token; password = " . $access_token . "; tenant = " . $tenant . PHP_EOL);
     //Write needed credentials of IM and VMRC
     fwrite($gestor, "type = InfrastructureManager; username = " . random_string(8) . "; password = " . random_string(10). PHP_EOL);
